a59302225d9135ef9519296615527b5d93d23cd3
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2022-04-29 11:11:45 +0530

    add not found gif

--- src/components/EXPENSES/NewComp.js
+++ src/components/EXPENSES/NewComp.js
@@ -10,17 +10,19 @@ const NewComp = (props) => {
   const addYearFilter = value =>{
     setFilterYear(value);
   }
-
+ 
  const dataArray = props.items.filter(item => item.date.getFullYear().toString() === filterYear)
+
+ let expenseContent = <img className='notFound__box' src='https://media.giphy.com/media/QTb0bukuuPe0GAvmVD/giphy.gif'/>
+ if(dataArray.length > 0){
+   expenseContent =  dataArray.map(item => <ExpenseItem
+    title={item.title} amount={item.amount} date={item.date} key={item.id} ></ExpenseItem> )
+ }
   return (
     <div>
     <ExpenseFilter selectValue={filterYear} setYearFilter={addYearFilter} />
     <Card className='expense__box'>
-    {
-     
-      dataArray.map(item => <ExpenseItem
-      title={item.title} amount={item.amount} date={item.date} key={item.id} ></ExpenseItem> )
-    }
+   {expenseContent}
     </Card>
     </div>
   )
--- src/components/filter/ExpenseFilter.js
+++ src/components/filter/ExpenseFilter.js
@@ -9,6 +9,7 @@ const ExpenseFilter = (props) => {
     <div>
         <h1>filter</h1>
         <select value={props.selectValue} onChange={fiterYearHandler}>
+        <option value='2019'>2019</option>
         <option value='2020'>2020</option>
         <option value='2021'>2021</option>
         <option value='2022'>2022</option>
